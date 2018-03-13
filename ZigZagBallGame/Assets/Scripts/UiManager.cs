4ece209af0d0adffb5360a000ad30c8e78474960
Jamie Cahn <jamie.cahn93@gmail.com>
2018-03-13 18:29:04 -0400

    version 1.0 - flappy game version 1.3 - zigzag ball game

---
+++ ZigZagBallGame/Assets/Scripts/UiManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour {
+
+    public static UiManager instance;
+
+    public GameObject zigzagPanel;
+    public GameObject gameOverPanel;
+    public GameObject tapText;
+    public Text score;
+    public Text highScore1;
+    public Text highScore2;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        highScore1.text = "High Score: " + PlayerPrefs.GetInt("highScore");		
+	}
+
+    public void GameStart()
+    {
+        tapText.SetActive(false);
+        zigzagPanel.GetComponent<Animator>().Play("PanelUp");
+    }
+
+    public void GameOver()
+    {
+        score.text = "Score: " + PlayerPrefs.GetInt("score");
+
+        highScore2.text = "High Score: " + PlayerPrefs.GetInt("highScore");
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
